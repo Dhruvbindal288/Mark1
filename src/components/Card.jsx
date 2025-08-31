cb1fe3209c67af7c765a3f451b83d877246482ec
Dhruvbindal288 <dhruvbindal288@gmail.com>
2025-08-31 21:54:25 +0530

    Card component created with dynamic content

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { FaBolt } from "react-icons/fa";
 
-function Card() {
+function Card({heading,text}) {
   return (
     <div className="w-full  md:w-80 lg:w-96 bg-green-100 rounded-3xl border-t-4 border-green-400 p-6 shadow-md hover:shadow-lg transition">
       <FaBolt className="text-yellow-400 text-3xl mb-2" />
       <h2 className="text-lg sm:text-xl md:text-2xl mb-4 font-medium">
-        Self-Sustaining Power
+       {heading}
       </h2>
       <p className="text-sm sm:text-base md:text-lg text-gray-600">
-        Creates a perpetual cycle of energy that intelligently utilizes power to
-        generate more, defining true sustainability.
+       {text}
       </p>
     </div>
   );
--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,13 +1,50 @@
-
-import Card from '../components/Card'
+import Card from "../components/Card";
 function Features() {
   return (
-    <div className='h-screen p-6'>
-      <h1 className='md:text-6xl text-center font-medium mb-10'>Innovative Features</h1>
-      <Card></Card>
-    
+    <div className="h-screen p-6">
+      <h1 className="md:text-6xl text-center font-medium mb-10">
+        Innovative Features
+      </h1>
+      <div className="flex gap-4 flex-wrap">
+        <Card
+          text={
+            "Creates a perpetual cycle of energy that intelligently utilizes power to generate more, defining true sustainability."
+          }
+          heading={"Self-Sustaining Power"}
+        />
+        <Card
+          text={
+            "Advanced dynamo control system allowing nuanced adjustment of engine speed for optimal performance."
+          }
+          heading={"Variable Speed Control"}
+        />
+        <Card
+          text={
+            "Transforms kinetic energy from slowdowns into power, maximizing efficiency and eco-consciousness."
+          }
+          heading={"Regenerative Braking"}
+        />
+        <Card
+          text={
+            "Brings electricity to remote areas, breaking free from traditional grids and fostering progress everywhere."
+          }
+          heading={"Off-Grid Power Generation"}
+        />
+        <Card
+          text={
+            "Comprehensive dashboard for monitoring energy generation, consumption, and efficiency metrics."
+          }
+          heading={"Real-Time Monitoring"}
+        />
+        <Card
+          text={
+            "Completely eco-friendly system that minimizes environmental impact while maximizing energy output."
+          }
+          heading={"Zero Carbon Emissions"}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Features
+export default Features;
